df761d993e86ad34fef4190b930d85d14f6663e8
Michael McCully <mike@mikemccully.com>
2012-05-28 22:11:25 -0500

    Unit test project

---
+++ FizzBuzzTestProject/FizzBuzzTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzTestProject
+{
+	[TestClass]
+	public class FizzBuzzTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
